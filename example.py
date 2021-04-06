1a7f00c6e12a52df6b73e508109a0bf5ebc67e71
Funkmonker <67586376+Funkmonker@users.noreply.github.com>
2021-04-06 12:03:57 +0200

    Update example.py

--- example.py
+++ example.py
@@ -2,10 +2,22 @@ from time import sleep
 
 from station import StationSimulator
 
-if __name__ == "__main__":
+# https://www.geeksforgeeks.org/python-merge-two-lists-into-list-of-tuples/
+def merge(list1, list2):
+      
+    merged_list = [(list1[i], list2[i]) for i in range(0, len(list1))]
+    return merged_list
+      
+# Driver code
 
+
+
+def get_List_of_Weather(interval_for_sim, sim_range, sim_sleep):
+    """interval for simulation in seconds
+    simulation range, amount of intervals
+    sleep period"""
     # Instantiate a station simulator
-    bergen_station = StationSimulator(simulation_interval=1)
+    bergen_station = StationSimulator(simulation_interval=interval_for_sim)
     # Turn on the simulator
     bergen_station.turn_on()
 
@@ -15,10 +27,10 @@ if __name__ == "__main__":
 
     # Capture data for 72 hours
     # Note that the simulation interval is 1 second
-    for _ in range(72):
+    for _ in range(sim_range):
         # Sleep for 1 second to wait for new weather data
         # to be simulated
-        sleep(1)
+        sleep(sim_sleep)
         # Read new weather data and append it to the
         # corresponding list
         temperature.append(bergen_station.temperature)
@@ -27,7 +39,10 @@ if __name__ == "__main__":
     # Shut down the simulation
     bergen_station.shut_down()
 
+    new_list = merge(temperature, precipitation)
+    return new_list
+    #print (new_list)
     # Print the collected data
-    print("Temperature\tPrecipitation")
-    for t, p in zip(temperature, precipitation):
-        print(t, "\t\t", p)
+    # print("Temperature\tPrecipitation")
+    # for t, p in zip(temperature, precipitation):
+    #     print(t, "\t\t", p)
