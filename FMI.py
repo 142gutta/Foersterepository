5ec9d787190bd2a9a95621f81622ba802e6d510f
Funkmonker <67586376+Funkmonker@users.noreply.github.com>
2021-04-06 14:12:48 +0200

    Update FMI.py

--- FMI.py
+++ FMI.py
@@ -5,9 +5,11 @@
 
 
 import socket
-Header = 64     #Vet ikke om denne er nødvendig
-Port = 5500
+Header = 1024     #Vet ikke om denne er nødvendig
+Port = 5501
 StorageServer = socket.gethostbyname(socket.gethostname()) #Fra Youtube-video, denne finner riktig IP-adresse på alle PC'er.
+#StorageServer = "10.111.24.92"
+print(StorageServer)
 Connection = (StorageServer, Port)
 DisconnectMessage = "!Bye"
 #GetWeather = '!Weather' #Request Weather info
@@ -16,15 +18,15 @@ DisconnectMessage = "!Bye"
 #Setter opp selve socketen, SOCK_Stream indikerer TCP socket.
 FMIclient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 FMIclient.connect(Connection)
-if connected:
-    print('Connected to server')
-else:
-    print("not connected")
+#if connected:
+#    print('Connected to server')
+#else:
+#    print("not connected")
 
-while (text:= input(">")).lower() != DisconnectMessage:                 #Vettafaen hva dette er men  la inn en standard send & receive greie fra en annen forelesning, denne må vi nok 
-    FMIclient.sendto(text.encode(), StorageServer)                     #endre, da vi ønsker å motta en liste/fil/noe annet, mulig vi må bruke "pickle" modulen til dette. 
+while (text:= input(">")).lower() != DisconnectMessage:                 #usikker, hva dette er men  la inn en standard send & receive greie fra en annen forelesning, denne må vi nok 
+    FMIclient.sendto(text.encode("UTF-8"), Connection)                     #endre, da vi ønsker å motta en liste/fil/noe annet, mulig vi må bruke "pickle" modulen til dette. 
     msg, addr = FMIclient.recvfrom(2048)
-    print(f"{addr[0]} says: {msg.decode()}")
+    print(f"{StorageServer} says: {msg.decode()}")
 
 
 
