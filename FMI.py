d01144e4a22da540afefe84a90d18ed48e7cdea4
Funkmonker <67586376+Funkmonker@users.noreply.github.com>
2021-04-06 16:51:30 +0200

    Update FMI.py

--- FMI.py
+++ FMI.py
@@ -23,7 +23,7 @@ FMIclient.connect(Connection)
 #else:
 #    print("not connected")
 
-while (text:= input(">")).lower() != DisconnectMessage:                 #usikker, hva dette er men  la inn en standard send & receive greie fra en annen forelesning, denne må vi nok 
+while (text:= input(">")).lower() != DisconnectMessage:                 #Vettafaen hva dette er men  la inn en standard send & receive greie fra en annen forelesning, denne må vi nok 
     FMIclient.sendto(text.encode("UTF-8"), Connection)                     #endre, da vi ønsker å motta en liste/fil/noe annet, mulig vi må bruke "pickle" modulen til dette. 
     msg, addr = FMIclient.recvfrom(2048)
     print(f"{StorageServer} says: {msg.decode()}")
