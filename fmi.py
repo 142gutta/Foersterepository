2083c152a07b5b4fd2a1d0c9e4b5f32dde47e135
Joans-LAN <81418411+Joans-LAN@users.noreply.github.com>
2021-04-08 15:37:50 +0200

    Add files via upload

--- fmi.py
+++ fmi.py
@@ -1,46 +1,50 @@
 import socket
-import json
 from time import sleep
+import pickle
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12342
 all_data = []
 tempreturedata = []
 raindata = []
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    command = ''
-    s.connect((HOST, PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+command = ''
+s.connect((HOST, PORT))
 
-    while command != "exit":
 
-        command = input("Available commands:\n1. 'all' - Returns all data\n2. 'exit' - Exits CLI\nCommand: ")
-
-        if command == "all":
-            s.send(command.encode("utf-8"))
-            response = s.recv(100000)
-            print(json.loads(response))
-            """
-            all_data.append(json.loads(response))
-            
-            split_list = [i.split() for i in all_data]
-            print(all_data)
-            print(len(split_list))
-            
-            for i in range(len(all_data)):
-                if i % 2 == 0:
-                    raindata.append(all_data[i])
-                else:
-                    tempreturedata.append(all_data[i])
-        print("tempreturedata\raindata")
+def view_forecast():
+    s.send(command.encode("utf-8"))
+    response = s.recv(1024)
+        
+    for i in range(len(pickle.loads(response))):
+        if i % 2 == 0:
+            tempreturedata.append(pickle.loads(response)[i])
+        else:
+            raindata.append(pickle.loads(response)[i])
+              
+while True:
+    command = input("Available commands:\n1. 'all' - Returns all data\n3. 'current' returns newest data\n2. 'exit' - Exits CLI\nCommand: ")
+    if command == "all":
+        view_forecast()
+        print("tempreturedata\traindata")
         for t, p in zip(tempreturedata, raindata):
             print(t, "\t\t", p)
-"""
-           
-        else:
-            print("Not a valid command")
+            tempreturedata = []
+            raindata = []
 
-    # with conn:
-    #     print("Connected:", addr)
-    #     while True:
-    #         data = conn.recv(1024)
-    #         print(json.loads(data))
+
+    elif command == "current":
+        view_forecast()
+        print(f"Here you have the newest forecast: < {tempreturedata[-1]}  celsius > and < {raindata[-1]} mm > of rain.")
+        tempreturedata = []
+        raindata = []
+
+        
+    elif command == "exit":
+        print("hope you liked it bye")
+        break
+
+    elif command != "current" or "all":
+        print("Not a command")
+
+
--- weather_station.py
+++ weather_station.py
@@ -18,26 +18,12 @@ if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 
         while True:
-            #Data3 = {"Temp": [] , "Rain": [], }
-
 
             d = bergen_station.temperature
-            e = bergen_station.rain
-            #f = (d, e)
 
             data = json.dumps(d).encode('utf-8')
-            data2 = json.dumps(e).encode('utf-8')
-            #data5 = json.dumps(f).encode('utf-8')
-
-            #Data3["Temp"].append(d)
-            #Data3["Rain"].append(e)
-
-            #data4 = json.dumps(Data3).encode('utf-8')
             
             s.sendto(data, (HOST, PORT))
-            s.sendto(data2, (HOST, PORT))
-            #s.sendto(data4, (HOST, PORT))
-            #s.sendto(data5, (HOST, PORT))
 
             sleep(SIM_INT)
 
